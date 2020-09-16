bbe2218399b1421d3708d213ed429736aa6b5d94
TheLippyT <44298235+TheLippyT@users.noreply.github.com>
2020-09-15 22:34:13 -0300

    Update script.sql
    many changes

--- script.sql
+++ script.sql
@@ -1,16 +1,12 @@
-/*creating all the tables*/
-
-/*create a table for the customers*/
 create table klanten
 (
 klanten_id number(11) not null primary key,
 klanten_naam varchar2(60),
 klanten_adress varchar2(60),
 klanten_city varchar2(60),
-klanten_birthdate date,
+klanten_birthdate date
 );
-
-/*create tables for the accounts spaar/giro*/
+/
 create table chequing_account
 (
 chequing_id number(11) not null primary key,
@@ -19,7 +15,7 @@ chequing_rekening_nummer number(11) not null
 alter table chequing_account
 add check (chequing_rekening_nummer >= 10000000000)
 add unique (chequing_rekening_nummer);
-
+/
 create table savings_account
 (
 savings_id number(11) not null primary key,
@@ -28,8 +24,7 @@ savings_rekening_nummer number(11) not null
 alter table savings_account
 add check (savings_rekening_nummer >= 10000000000)
 add unique (savings_rekening_nummer);
-
-/*table to link klanten en accounts*/
+/
 create table klanten_chequing
 (
 klanten_account_id number(11) not null primary key,
@@ -46,8 +41,7 @@ savings_id number(11),
 constraint fk_ks_link foreign key (klanten_id) references klanten(klanten_id),
 constraint fk_sk_link foreign key (savings_id) references savings_account(savings_id)
 );
-
-/*account balanses*/
+/
 create table chequing_balans
 (
 balans_id number(11) not null primary key,
@@ -67,17 +61,6 @@ last_interaction date,
 constraint fk_sa foreign key (klanten_account_id) references klanten_savings(klanten_account_id)
 );
 /
-/*inserting data into the tables*/
-insert into klanten (klanten_id, klanten_naam, klanten_adress, klanten_city, klanten_birthdate)
-VALUES (1, 'john', 'bacovenstraat 100', 'paramaribo', '02-JAN-1980');
-/
-INSERT ALL
- INTO klanten (klanten_id, klanten_naam, klanten_adress, klanten_city, klanten_birthdate) VALUES (2, 'johanna', 'bananenstraat 120', 'paramaribo', '02-JAN-1980')
- INTO klanten (klanten_id, klanten_naam, klanten_adress, klanten_city, klanten_birthdate) VALUES (3, 'jason', 'ananasstraat 99', 'paramaribo', '02-JAN-1980')
- SELECT * FROM klanten; 
-/
-
-
 create table klanten_jn as select * from klanten where 1=0;
 /
 alter table klanten_jn
@@ -105,7 +88,6 @@ begin
       :new.klanten_adress,
       :new.klanten_city,
       :new.klanten_birthdate,
-      :new.klanten_registration_age,
       'I');
       
 end;
@@ -125,7 +107,6 @@ begin
      klanten_adress,
      klanten_city,
      klanten_birthdate,
-     klanten_registration_age,
      dml_actie
      )
      values
@@ -134,7 +115,6 @@ begin
       :new.klanten_adress,
       :new.klanten_city,
       :new.klanten_birthdate,
-      :new.klanten_registration_age,
       'D');
       
 end;
@@ -153,7 +133,6 @@ begin
      klanten_adress,
      klanten_city,
      klanten_birthdate,
-     klanten_registration_age,
      dml_actie
      )
      values
@@ -162,12 +141,9 @@ begin
       :new.klanten_adress,
       :new.klanten_city,
       :new.klanten_birthdate,
-      :new.klanten_registration_age,
       'U');
       
 end;
-/
-/*update statement*/
 /
 create or replace procedure calculation_interest
 is
@@ -178,7 +154,7 @@ begin
 end;
 /
 begin
-    dbms_scheduler.create_job (
+    DBMS_SCHEDULER.create_job (
         job_name => 'interest_calculation',
         job_type => 'PL/SQL BLOCK',
         job_action => 'begin calculation_interest; end;',
@@ -186,12 +162,3 @@ begin
         repeat_interval => 'FREQ=MONTHLY; BYMONTHDAY=-1',
         enable => true);
  end;
- /
- /*views*/
- CREATE or REPLACE VIEW klanten_account 
-    SELECT k.klanten_id, k.klanten_naam, ca.chequing_id, ca.chequing_rekening_nummer
-    FROM klanten k
-    INNER JOIN klanten_chequing kc
-    ON k.klanten_id = kc.klanten_id
-    INNER JOIN klanten_chequing kc
-    ON ca.chequing_id = kc.chequing_id;
